35fb1e81a735c1c15750ea0e87f09516b0fcaa46
Jihun Choi <jhchoi@europa.snu.ac.kr>
2017-07-20 11:13:50 +0900

    title

--- arxiv/arxiv.py
+++ arxiv/arxiv.py
@@ -13,15 +13,18 @@ from requests.exceptions import HTTPError
 root_url = 'http://export.arxiv.org/api/'
 
 
-def query(s_all, prune=True, start=0, max_results=10, category=None, author=None):
+def query(s_all, prune=True, start=0, max_results=10, category=None, author=None, title=None):
     s = "all:{}".format(s_all)
     if category is not None:
         s += " AND cat:{}".format(category)
     if author is not None:
         s += " AND au:{}".format(author)
 
-    # Gets a list of top results, each of which is a dict
-    results = feedparser.parse(root_url + 'query?search_query=' + quote_plus(s) + '&start=' + str(start) + '&max_results=' + str(max_results))
+    if title is not None:
+        s += " AND ti:{}".format(title)
+
+    # Gets a list of top results, eastat.AP Statistics - Applications
+   results = feedparser.parse(root_url + 'query?search_query=' + quote_plus(s) + '&start=' + str(start) + '&max_results=' + str(max_results))
     if results.get('status') != 200:
         # TODO: better error reporting
         raise Exception("HTTP Error " + str(results.get('status', 'no status')) + " in query")
